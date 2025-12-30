9037c05e96b1dba6c5bdc47d01e18e75a7200b69
Zakibelm <zakibelm66@gmail.com>
2025-12-29 21:46:17 -0500

    fix(client): enable superjson transformer to match server config

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,10 +1,12 @@
 import { createTRPCReact } from "@trpc/react-query";
 import { httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../../../server/routers";
+import superjson from "superjson";
 
 export const trpc = createTRPCReact<AppRouter>();
 
 export const trpcClient = trpc.createClient({
+    transformer: superjson,
     links: [
         httpBatchLink({
             url: "/api/trpc",
